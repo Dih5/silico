aebb18230b6a1199731fe26a522836eb40031440
dih5 <dihedralfive@gmail.com>
2022-03-07 18:21:49 +0100

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -38,6 +38,6 @@ setup(author="Dih5",
       packages=find_packages(include=['silico'], exclude=["demos", "tests", "docs"]),
       install_requires=["numpy", "pandas", "scipy", "scikit-learn", "click", "tqdm"],
       url='https://github.com/Dih5/silico',
-      version='0.1.1',
+      version='0.1.2',
 
       )
