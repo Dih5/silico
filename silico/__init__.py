b486318f5488c02a4fec712f4fd10d3b39feb220
Dih5 <dihedralfive@gmail.com>
2024-10-09 11:18:49 +0200

    Bump version: 0.2.0 → 0.2.1

--- silico/__init__.py
+++ silico/__init__.py
@@ -1,6 +1,6 @@
 """silico - Python package to handle in silico experiments"""
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 __author__ = 'Dih5 <dihedralfive@gmail.com>'
 
 from .base import Experiment, Variable, SubExperiment
